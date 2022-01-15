395d2c71eca37c95f47a74e5591add7a2595f11c
jarencudilla <jarencudilla@gmail.com>
2022-01-16 03:01:44 +0800

    2nd full commit - after multiple installation, setup of server and installation of multiple modules

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,17 @@
 import express = require( "express" );
+import path from "path";
+
 const app = express();
 const port =  8080;
 
+app.set( "views", path.join(__dirname, "views"));
+app.set( "view engine", "ejs" );
+
 app.get ( "/", ( req, res ) => {
-   res.send( "Hello World!" );
+   res.render( "index" );
 });
 
 app.listen( port, () => {
   // tslint:disable-next-line:no-console
-  console.log( `server started at http://localhost: ${ port }`);
+  console.log( `server started at http://localhost:${ port }`);
 });
